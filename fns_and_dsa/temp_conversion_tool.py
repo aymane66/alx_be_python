a21e8a6d484014612f48365277e2e5d58ae1243a
Ayman <ayman.mouhcine1995@gmail.com>
2025-06-08 23:39:57 +0100

    weather unit converter

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+
+def convert_to_celsius(fahrenheit):
+    total = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return f"{fahrenheit}°F is {total}°C"
+
+def convert_to_fahrenheit(celsius):
+    total = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return f"{celsius}°C is {total}°F"
+
+
+
+temperatue = int(input("Enter the temperature to convert: "))
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+if unit == "C":
+    print(convert_to_fahrenheit(temperatue))
+elif unit == "F":
+    print(convert_to_celsius(temperatue))
+else:
+    print("Invalid unit. Choose C or F")
