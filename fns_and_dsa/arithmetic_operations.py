4284305e47e8b8be74eff925b9506d3fb803e95c
Ayman <ayman.mouhcine1995@gmail.com>
2025-06-08 20:00:47 +0100

    Calculator with funtion

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
-def perform_operation (num1, num2, operation):
-    
-    if operation == "add":
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
         return num1 + num2
-    elif operation == "subtract":
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == "multiply":
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == "divide":
-        if num2 == 0:
-            print("Division by 0 is not permitted.")
-        else:    
+    elif operation == 'divide':
+        if num2 != 0:
             return num1 / num2
-        
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Unsupported operation"
