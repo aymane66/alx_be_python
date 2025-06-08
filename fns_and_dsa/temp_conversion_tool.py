71c90ba10eae0a14dc1446ebbcbd6095c9dcb25f
Ayman <ayman.mouhcine1995@gmail.com>
2025-06-08 23:46:16 +0100

    weather unit converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     total = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -10,14 +9,15 @@ def convert_to_fahrenheit(celsius):
     total = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return f"{celsius}°C is {total}°F"
 
+try:
+    temperature = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
-
-temperatue = int(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-
-if unit == "C":
-    print(convert_to_fahrenheit(temperatue))
-elif unit == "F":
-    print(convert_to_celsius(temperatue))
-else:
-    print("Invalid unit. Choose C or F")
+    if unit == "C":
+        print(convert_to_fahrenheit(temperature))
+    elif unit == "F":
+        print(convert_to_celsius(temperature))
+    else:
+        print("Invalid unit. Choose C or F")
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
